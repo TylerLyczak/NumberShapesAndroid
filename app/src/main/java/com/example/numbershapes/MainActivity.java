a542b9eb8e05a352f0b3467c40755d7aebd6d487
TylerLyczak <tyler.m.lyczak@gmail.com>
2019-11-21 02:13:31 -0600

    added functionality

--- app/src/main/java/com/example/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/numbershapes/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.numbershapes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public class myNum {
-        int userNum;
+        double userNum;
 
-        public myNum (int num)  {
+        public myNum (double num)  {
             this.userNum = num;
+        }
+
+        public double getUserNum()   {
+            return userNum;
         }
 
         public boolean isSquare()   {
@@ -38,6 +45,44 @@ public class MainActivity extends AppCompatActivity {
 
             // If the number can divide evenly with 1, then its a triangular number
             return (testNum % 1 == 0);
+        }
+    }
+
+
+    public void onClick (View view) {
+
+        EditText editText = (EditText)findViewById(R.id.userText);
+        String userText = editText.getText().toString();
+        double userDouble = Double.parseDouble(userText);
+
+        myNum userNum = new myNum(userDouble);
+
+        boolean checkSquare = userNum.isSquare();
+        boolean checkTri = userNum.isTriangular();
+
+        // Number is both not square and triangular
+        if (!checkSquare && !checkTri)  {
+            String msg = userText + " is not Square nor Triangular";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        // Number is square, but not triangular
+        else if (checkSquare && !checkTri)  {
+            String msg = userText + " is Square";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        // Number is triangular but not square
+        else if (!checkSquare && checkTri)  {
+            String msg = userText + " is Triangular";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        // Number is both
+        else if (checkSquare && checkTri)   {
+            String msg = userText + " is both Square and Triangular";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        // ERROR
+        else    {
+            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
         }
     }
 
